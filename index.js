2ab12dc71fb00214e0746d62fbaad99d9c564608
bigsteppa05 <habibforderiv05@gmail.com>
2024-03-19 10:01:51 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
-}
+  describe('scuberGreetingForFeet()', function () {
+    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+    });
+
+    it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
+      expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.');
+    });
+
+    it('charges 30 dollars for a distance over 2000 feet', function () {
+      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+    });
+
+    it('does not allow rides over 2500 feet', function () {
+      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+    });
+  });
+};
+
 
 function ternaryCheckCity(){
-  // Write your code here!
+ if (city === 'NYC') {
+   return 'Ok, sounds good.';
+ } else {
+   return 'No go.';
+ }
 }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === 'generous') {
+    return 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    return 'Bye.';
+  }
 }
